37e22a481c09554801216d5e42b58918a9add31e
wyb <2457059817@qq.com>
2016-12-07 12:16:51 +0800

    @Autowired自动注入

--- src/main/java/com/wyb/controller/TestController.java
+++ src/main/java/com/wyb/controller/TestController.java
@@ -5,28 +5,52 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class IndexController {
+import com.wyb.service.UserServiceI;
 
-    private static final Logger logger = Logger.getLogger(IndexController.class);
+@Controller
+@RequestMapping("/testController")
+public class TestController {
+
+	@Autowired
+	private UserServiceI userService; 
+	
+    private static final Logger logger = Logger.getLogger(TestController.class);
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String method(){
+    	System.out.println("根方法");
+    	return "";
+    }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String index(HttpServletRequest request, HttpServletResponse response,Model m) {
         //ModelAndView view = new ModelAndView("manage/login");
        // view.addObject("c", "好 ");
         request.setAttribute("param1", "hello");
-      
+        userService.test();
+        //System.out.println("path:"+request.getContextPath());
     	m.addAttribute("param2", "test");
         logger.info("开始进入页面");
 
        // return view;
         return "jsp/hello";
     }
+    
+    @RequestMapping(value="/login",method=RequestMethod.GET)
+    public String login(){
+    	
+    	return "jsp/login";
+    }
 
+    @RequestMapping(value="loginCheck",method=RequestMethod.POST )
+    public String loginCheck(){
+    	return "";
+    }
 }
